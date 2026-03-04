0d28544c35d04312dcbcdc678796ee59d7a32309
Marc Galang <marc.galang@wunder.io>
2026-03-04 17:02:18 +0200

    feat: remove unused Link import from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
